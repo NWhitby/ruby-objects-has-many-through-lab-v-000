02e33262a26a431943778fad3ea71e0f5f976e19
NaKia Whitby <whitby.nakia@gmail.com>
2019-01-01 19:30:14 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,17 +1,18 @@
 class Appointment
 
-     attr_accessor :patient, :doctor, :date
-    @@all = []
+  attr_accessor :patient, :doctor, :date
 
-     def initialize(patient, doctor, date)
-     @patient = patient
-     @doctor = doctor
-     @date = date
-     @@all << self
-   end
+  @@all = []
 
-     def self.all
-     @@all
-   end
-
+  def initialize(patient, doctor, date)
+    @patient = patient
+    @doctor = doctor
+    @date = date
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+end
