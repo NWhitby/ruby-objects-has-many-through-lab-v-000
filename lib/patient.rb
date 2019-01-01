244375e23b948f7e43f84fab90f6077d55a69566
NaKia Whitby <whitby.nakia@gmail.com>
2019-01-01 19:32:25 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,31 +1,28 @@
 class Patient
 
-     attr_accessor :name, :appointment, :doctor
-    @@all = []
+  attr_accessor :name, :appointment, :doctor
 
-     def initialize(name)
-     @name = name
-     @@all << self
-   end
+  @@all = []
 
-    def self.all
-     @@all
-   end
-
-     def new_appointment(doctor, date)
-     Appointment.new(self, doctor, date)
-   end
-
-     def appointments
-     Appointment.all.select do |appointment|
-       appointment.patient == self
-     end
-   end
-
-     def doctors
-     Appointment.all.collect do |appointment|
-       appointment.doctor
-     end
-   end
-
+  def initialize(name)
+    @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
+  end
+
+  def appointments
+    Appointment.all.select { |appointment| appointment.patient == self }
+  end
+
+  def doctors
+    Appointment.all.collect { |appointment| appointment.doctor }    
+  end
+
+end
