2fce80c4facbc23f63771fb81b7be8115ce5e62e
NaKia Whitby <whitby.nakia@gmail.com>
2019-01-01 19:26:56 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,27 +1,28 @@
 class Genre
 
-    attr_accessor :name
-   @@all = []
+  attr_accessor :name
 
-    def initialize(name)
-     @name = name
-     @@all << self
-   end
+  @@all = []
 
-    def self. all
-     @@all
-   end
-
-    def songs
-     Song.all.select do |songs|
-       songs.genre == self
-     end
-   end
-
-    def artists
-     Song.all.collect do |songs|
-       songs.artist
-     end
-   end
-
+  def initialize(name)
+   @name = name
+   @@all << self
   end
+
+  def self. all
+    @@all
+  end
+
+  def songs
+    Song.all.select do |songs|
+      songs.genre == self
+    end
+  end
+
+  def artists
+    Song.all.collect do |songs|
+      songs.artist
+    end
+  end
+
+end
